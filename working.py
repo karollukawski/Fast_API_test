9fb2bfce4242ec25ce3dba45ff197534198ad78e
karollukawski <krl.lukawski@gmail.com>
2024-02-18 14:37:12 +0100

    Post method other approach (Query method)

--- working.py
+++ working.py
@@ -9,23 +9,17 @@ class Item(BaseModel):
     price: float
     brand: Optional[str] = None
 
-inventory = {
-    1: {
-        "name": "Honey",
-        "price": "8.99",
-        "brand": "ABC"
-    }
-}
+inventory = {}
 
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(description = "The ID of the item to retrive", gt = 0, le = 5)):
     return inventory [item_id]
 
 # URL: http://127.0.0.1:8000/get-by-name/1?test=2&name=Honey <- order doesn't matter
-@app.get("/get-by-name/{item_id}")
-def get_item(*, item_id: int, name: Optional[str] = None, test: int):
-    for id in inventory:
-        if inventory[item_id]["name"] == name:
+@app.get("/get-by-name")
+def get_item(name: str = Query(title="Name", description="Name of item.", max_length=10, min_length=2)):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data":"Not found"}
 
